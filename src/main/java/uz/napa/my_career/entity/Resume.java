8a82f2a801eebdfeb8f05f1114cd224d80185d5d
Abdu880 <muzaffarxonov2326@gmail.com>
2021-10-26 20:03:47 +0500

    New Changes

--- src/main/java/uz/napa/my_career/entity/Resume.java
+++ src/main/java/uz/napa/my_career/entity/Resume.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,32 +16,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Resume {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = ("firstName"))
-    private String firstName;
-    @Column(name = ("lastNAme"))
-    private String lastName;
-    @Column
-    private String address;
-    @Column(name = ("phoneNumber"))
-    private String phoneNumber;
-    @Column
-    private String email;
-    @Column(name = ("cratedDate"))
-    private LocalDateTime cratedDate;
+
     @Column(columnDefinition = "text")
     private String bio;
+
     @Column(columnDefinition = "text")
     private String interests;
+
     @ManyToOne
     private User user;
-    @OneToMany
-    private List<Skills> skills;
-    @OneToMany
-    private List<Education> educations;
-    @OneToMany
-    private List<Experience> experiences;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Skills> skills;
 }
